44ff1e899cfc72f02d8291bc50bc5a8a9fbef8a8
Magara Etchien N'gouan <magaraetchien@live.fr>
2022-12-28 10:54:48 +0000

    DI_BootCamp Week2 Day1 Daily Challenge

---
+++ exercice1.js
@@ -0,0 +1,25 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//1.Remove Banana from the array.
+fruits.shift(fruits)  
+
+//console.log(fruits);
+
+//2 Sort the array in alphabetical order.
+fruits.sort() 
+
+//console.log(fruits);
+
+//3. Add “Kiwi” to the end of the array.
+
+fruits.push(kiwi)
+
+//console.log(fruits);
+
+//4. Remove “Apples” from the array. Don’t use the same method as in part 1.
+fruits.splice(0, 1);
+
+//5-Sort the array in reverse order
+fruits.reverse();
+//console.log(fruits);
+
---
+++ exercice2.js
@@ -0,0 +1,5 @@
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+//1. Access and then console.log “Oranges”.
+
+console.log(moreFruits[1][1][2])
