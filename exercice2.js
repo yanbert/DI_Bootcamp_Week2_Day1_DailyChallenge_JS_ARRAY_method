0d7f8b05df62cfbd84afc935cbdaf84332648b8d
yanbert <yanbert@gmail.com>
2022-12-30 01:04:20 +0000

    Merci de voir mes commentaires dans le code

--- exercice2.js
+++ exercice2.js
@@ -1,5 +1,8 @@
 const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
 //1. Access and then console.log “Oranges”.
-
+/***************************************************
+Correcteur: Yannick GOUGUIA
+Merci de revoir cet exercice
+***************************************************/
 console.log(moreFruits[1][1][2])
